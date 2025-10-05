7c2c79820d858f9e53b0b59adba64fbdcf019c3d
dolodmg <oranotherway@gmail.com>
2025-10-05 19:41:13 -0300

    log en apiPayments

--- src/lib/api/apiPayments.js
+++ src/lib/api/apiPayments.js
@@ -2,6 +2,8 @@ import 'server-only'
 import { apiRequest } from '@/lib/apiRequest';
 
 const BASE_URL = `${process.env.JAVA_BACKEND_URL}/api/mercadopago`;
+console.log('🔍 JAVA_BACKEND_URL:', process.env.JAVA_BACKEND_URL);
+console.log('🔍 BASE_URL completa:', BASE_URL);
 
 export async function verifyPayment(paymentId) { 
     return await apiRequest(`${BASE_URL}/verify?paymentId=${paymentId}`, 'GET', null, 'application/json', false);
