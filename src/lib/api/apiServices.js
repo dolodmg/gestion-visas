b507987c9e09342524f1da47b3deed5a865d4fa6
dolodmg <oranotherway@gmail.com>
2025-10-05 19:01:57 -0300

    fix en la url de mp

--- src/lib/api/apiServices.js
+++ src/lib/api/apiServices.js
@@ -1,7 +1,7 @@
 import 'server-only'
 import { apiRequest } from '@/lib/apiRequest';
 
-const BASE_URL = `${process.env.NEXT_PUBLIC_JAVA_BACKEND_URL}/api/services`;
+const BASE_URL = `${process.env.JAVA_BACKEND_URL}/api/services`;
 
 export async function getService(idService) { 
     return await apiRequest(`${BASE_URL}/${idService}`, 'GET', null, 'application/json', false);
